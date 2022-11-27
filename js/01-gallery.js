210fa72529927b633d6aaaf4ad6510806a39ad0f
Alex-byte256 <oleksandr.kozak.2019@gmail.com>
2022-11-27 15:25:25 +0300

    make mark up and add  b

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkUp = galleryItems
+  .map(
+    (el) => `
+	<div class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>
+</div>
+	`
+  )
+  .join("");
+
+galleryRef.innerHTML = galleryMarkUp;
